a229065ca837d5fcad3a157982b157da80bc9662
Armando Rivas <rivasarmando271084@gmail.com>
2023-11-14 07:17:59 -0600

    Added botttracp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    NgbDropdownModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
