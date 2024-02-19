3f1f789d421e6db39afa960446bc21c8a5e59b77
Armando Rivas <rivasarmando271084@gmail.com>
2024-02-19 07:39:07 -0600

    Added initial project

---
+++ src/main/java/com/example/mythclothllectionui/MythClothllectionUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mythclothllectionui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythClothllectionUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythClothllectionUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mythclothllectionui/MythClothllectionUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mythclothllectionui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythClothllectionUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
